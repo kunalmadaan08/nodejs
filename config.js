b242b556686670b64911a7d5239fcd4ce701a87b
kunalmadaan08 <kunalmadaan08@gmail.com>
2019-09-27 01:17:00 +0530

    commit new changes

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv').config();
+
 var CONFIG = {
 	ZOHO_API_URL:'https://books.zoho.com/api/v3/contacts?organization_id='+process.env.ZOHO_ORGANIZATION_ID,
 	ZOHO_AUTHORIZATION_TOKEN: process.env.ZOHO_AUTHORIZATION_TOKEN	
--- server.js
+++ server.js
@@ -3,10 +3,14 @@ var app = express();
 var cors = require('cors')
 app.use(cors());
 
+var dotenv = require('dotenv').config();
+
 var contactController = require('./controllers/contact')
 
 app.get('/contacts', contactController.getContacts);
 
-app.listen(5000, function(){
-  console.log('Server started on port ' + 5000);
+var PORT = process.env.port || 5000;
+
+app.listen(PORT, function(){
+  console.log('Server started on port ' + PORT);
 });
